01abd7f8f554adcfe1c0b6008904c3857af4140c
Mates24 <matejcikmathias@gmail.com>
2024-09-15 10:59:19 +0200

    On medium devices after a link is clicked menu will hide

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,21 @@
-const menuBtn = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.links');
+const menuBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".links");
 
 var clickcounter = 0;
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener("click", () => {
+  clickcounter++;
+
+  if (clickcounter % 2 === 0) {
+    navLinks.style.transform = "translateX(200%)";
+  } else {
+    navLinks.style.transform = "translateX(0)";
+  }
+});
+
+navLinks.addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
+    navLinks.style.transform = "translateX(200%)";
     clickcounter++;
-    
-    if(clickcounter % 2 === 0){
-        navLinks.style.transform = "translateX(200%)";
-    }else{
-        navLinks.style.transform = "translateX(0)";
-    }
+  }
 });
